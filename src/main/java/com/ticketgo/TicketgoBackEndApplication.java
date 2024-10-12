ade3cffc8be4279ecabccc735bf62072bf001fac
tay <phuonggteyy@gmail.com>
2024-10-12 07:59:27 +0700

    #1 Create base code

---
+++ src/main/java/com/ticketgo/TicketgoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ticketgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketgoBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketgoBackEndApplication.class, args);
+    }
+
+}
