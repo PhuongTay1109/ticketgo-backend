c08807c0c0eae38ad0f9fe27e754dc1a5024bb40
tay <phuonggteyy@gmail.com>
2024-10-24 20:26:45 +0700

    #15 ScheduleMapper

---
+++ src/main/java/com/ticketgo/mapper/ScheduleMapper.java
@@ -0,0 +1,33 @@
+package com.ticketgo.mapper;
+
+import com.ticketgo.dto.ScheduleDTO;
+import com.ticketgo.model.Schedule;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.time.Duration;
+
+@Mapper
+public interface ScheduleMapper {
+    ScheduleMapper INSTANCE = Mappers.getMapper(ScheduleMapper.class);
+
+    @Mapping(target = "routeName", source = "schedule.route.routeName")
+    @Mapping(target = "busImage", source = "schedule.bus.busImage")
+    @Mapping(target = "busType", source = "schedule.bus.busType")
+    @Mapping(target = "departureLocation", source = "schedule.route.departureLocation")
+    @Mapping(target = "arrivalLocation", source = "schedule.route.arrivalLocation")
+    @Mapping(target = "departureTime", source = "schedule.departureTime")
+    @Mapping(target = "arrivalTime", source = "schedule.arrivalTime")
+    @Mapping(target = "availableSeats", source = "availableSeats")
+    @Mapping(target = "travelDuration", expression = "java(calculateTravelDuration(schedule))")
+    ScheduleDTO toScheduleDTO(Schedule schedule, int availableSeats);
+
+    default String calculateTravelDuration(Schedule schedule) {
+        Duration duration = Duration.between(schedule.getDepartureTime(), schedule.getArrivalTime());
+        long hours = duration.toHours();
+        long minutes = duration.toMinutes() % 60;
+        return String.format("%d giờ %d phút", hours, minutes);
+    }
+}
+
