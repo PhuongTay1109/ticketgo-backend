55792aa13b4007715ea557187ee79b5c464dcad0
tay <phuonggteyy@gmail.com>
2024-12-05 08:17:22 +0700

    change return url

--- src/main/java/com/ticketgo/controller/PaymentController.java
+++ src/main/java/com/ticketgo/controller/PaymentController.java
@@ -6,6 +6,7 @@ import com.ticketgo.service.EmailService;
 import com.ticketgo.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,9 @@ public class PaymentController {
     private final PaymentService paymentService;
     private final BookingService bookingService;
     private final EmailService emailService;
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @PostMapping("vnpay")
     public String createVNPayment(@RequestBody PaymentRequest request) {
@@ -34,7 +38,7 @@ public class PaymentController {
             emailService.sendBookingInfo(Long.parseLong(orderInfo));
             return ResponseEntity.status(302)
                     .header("Location",
-                            "http://localhost:3000/thankyou")
+                            frontendUrl + "/thankyou")
                     .build();
         } else {
             bookingService.setFailedVNPayBooking(Long.parseLong(orderInfo));
