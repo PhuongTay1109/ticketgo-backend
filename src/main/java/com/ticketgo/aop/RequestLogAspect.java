f3a53c8792359f945e26efb0c4882f4fe83fa66e
tay <phuonggteyy@gmail.com>
2025-02-19 20:12:03 +0700

    add AOP for logging

---
+++ src/main/java/com/ticketgo/aop/RequestLogAspect.java
@@ -0,0 +1,69 @@
+package com.ticketgo.aop;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Order(1)
+@Slf4j
+public class RequestLogAspect {
+    @Around("@within(org.springframework.web.bind.annotation.RestController)")
+    public Object controllerLogAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        return logExecutionTime(joinPoint);
+    }
+
+    private Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+                .currentRequestAttributes())
+                .getRequest();
+
+        String requestPath = request.getRequestURI();
+        String httpMethod = request.getMethod();
+
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+
+        log.info("[RequestLog-REQUEST] {} {} - {}.{} - Input parameters: {}",
+                httpMethod,
+                requestPath,
+                className,
+                methodName,
+                Arrays.toString(args));
+
+        long startTime = System.currentTimeMillis();
+        Object result = null;
+
+        try {
+            result = joinPoint.proceed();
+
+            log.info("[RequestLog-RESPONSE] {} {} - {}.{} - Execution time: {} ms - Output: {}",
+                    httpMethod,
+                    requestPath,
+                    className,
+                    methodName,
+                    System.currentTimeMillis() - startTime,
+                    result.toString());
+        } catch (Exception e){
+            log.info("[RequestLog-RESPONSE] {} {} - {}.{} - Execution time: {} ms - Error: {}",
+                    httpMethod,
+                    requestPath,
+                    className,
+                    methodName,
+                    System.currentTimeMillis() - startTime,
+                    e.getMessage());
+        }
+
+        return result;
+    }
+}
